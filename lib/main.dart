028a4eccfe63f5aa77620f27ac90663dcd7b0464
Roger Paris <roger.parisvicent@gmail.com>
2020-01-29 17:09:54 +0100

    adds logic for adding transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:expense_manager/widgets/transaction_list.dart';
+import 'package:expense_manager/widgets/user_transactions.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -14,9 +14,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  // String titleInput;
-  // String amountInput;
-
   final titleController = TextEditingController();
   final amountController = TextEditingController();
 
@@ -33,38 +30,7 @@ class MyHomePage extends StatelessWidget {
               child: Container(width: 100, child: Text('CHART!')),
               elevation: 5,
             ),
-            Card(
-              child: Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: <Widget>[
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Title'),
-                      controller: titleController,
-                      // onChanged: (value) {
-                      //   titleInput = value;
-                      // },
-                    ),
-                    TextField(
-                      decoration: InputDecoration(labelText: 'Amount'),
-                      controller: amountController,
-                      // onChanged: (value) {
-                      //   amountInput = value;
-                      // },
-                    ),
-                    FlatButton(
-                      child: Text('Add Transaction'),
-                      onPressed: () {
-                        print(titleController.text);
-                      },
-                      color: Colors.purple,
-                    )
-                  ],
-                ),
-              ),
-            ),
-            TransactionList()
+            UserTransactions()
           ],
         ));
   }
