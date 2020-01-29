6aac1b4d531c71d17d53df613d1360b342894c2b
Roger Paris <roger.parisvicent@gmail.com>
2020-01-29 16:02:58 +0100

    formats date and adds textinput fields

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:expense_manager/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,12 +31,32 @@ class MyHomePage extends StatelessWidget {
           title: Text('Flutter App'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Card(
               color: Colors.blue,
               child: Container(width: 100, child: Text('CHART!')),
               elevation: 5,
+            ),
+            Card(
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Title'),
+                    ),
+                    TextField(
+                      decoration: InputDecoration(labelText: 'Amount'),
+                    ),
+                    FlatButton(
+                      child: Text('Add Transaction'),
+                      onPressed: () {},
+                      color: Colors.purple,
+                    )
+                  ],
+                ),
+              ),
             ),
             Column(
               children: transactions.map((tx) {
@@ -49,7 +70,7 @@ class MyHomePage extends StatelessWidget {
                             border: Border.all(color: Colors.purple, width: 2)),
                         padding: EdgeInsets.all(10),
                         child: Text(
-                          tx.amount.toString(),
+                          '\$${tx.amount.toString()}',
                           style: TextStyle(
                               fontWeight: FontWeight.bold,
                               fontSize: 20,
@@ -65,7 +86,7 @@ class MyHomePage extends StatelessWidget {
                                 fontSize: 18, fontWeight: FontWeight.bold),
                           ),
                           Text(
-                            tx.date.toString(),
+                            DateFormat('yyyy-MM-dd').format(tx.date),
                             style: TextStyle(color: Colors.grey),
                           )
                         ],
