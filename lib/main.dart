0d057b23c42f144cc488969e367575760783d2d8
Roger Paris <roger.parisvicent@gmail.com>
2020-02-06 14:31:51 +0100

    prints chart data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:expense_manager/models/transaction.dart';
+import 'package:expense_manager/widgets/chart.dart';
 import 'package:expense_manager/widgets/new_transaction.dart';
 import 'package:expense_manager/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
@@ -44,6 +45,12 @@ class _MyHomePageState extends State<MyHomePage> {
     //     amount: 16.53,
     //     date: DateTime.now())
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(DateTime.now().subtract(Duration(days: 7)));
+    }).toList();
+  }
 
   void _addNewTransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
@@ -95,11 +102,7 @@ class _MyHomePageState extends State<MyHomePage> {
         body: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-              Card(
-                color: Colors.blue,
-                child: Container(width: 100, child: Text('CHART!')),
-                elevation: 5,
-              ),
+              Chart(_recentTransactions),
               TransactionList(_userTransactions)
             ],
           ),
